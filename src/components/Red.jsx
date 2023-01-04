d04d8dd0f7a76b2f18311692987dbf50f16efcd8
Julian Berton <julianbertonb@gmail.com>
2023-01-04 13:50:05 -0300

    saf

--- src/components/Red.jsx
+++ src/components/Red.jsx
@@ -15,6 +15,7 @@ const Red = () => {
             <div className="contenedor">
                 <h2>Red</h2>
                 
+
                 <div className="content">
                     <img src={imagen}></img>
                     <Redes />
