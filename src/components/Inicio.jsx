297a7c62fe7177a662dd7abb7dbe48a2e0e046bb
Julian Berton <julianbertonb@gmail.com>
2023-02-16 17:29:09 -0300

    comiteando

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,4 +1,5 @@
 import gif from "../img/gifinicio2.gif";
+import gif2 from "../img/Gifok.mp4";
 import logo_carlota from "../img/logo_carlota.webp";
 import estrella from "../img/estrella_amarilla.webp";
 
@@ -19,7 +20,10 @@ const Inicio = ({handleIdioma, idioma, cargoElsitio}) => {
                 <div className="img">
                     <img className="logo_i" src={logo_carlota}></img>
                     <div className="movil">
-                        <img onLoad={() => cargoElsitio()} className="foto" src={gif}></img>
+                        <video className="foto" width="50vw" height="auto" autoPlay muted loop>
+                            <source src={gif2} type="video/mp4"></source>
+                        </video>
+                        {/* <img onLoad={() => cargoElsitio()} className="foto" src={gif}></img> */}
                         <p>{inicio[idioma]["titulo"]}</p>
                     </div>
                     <p>{inicio[idioma]["titulo"]}</p>
